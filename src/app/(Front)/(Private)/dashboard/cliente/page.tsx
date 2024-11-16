51d341bf848f82e28f1186e3cd4a24b33aba029b
carveri <carvajal.cortes.e19@gmail.com>
2024-11-16 16:55:29 -0300

    v28 Tengo listo los clientes, agregue el visionario, ahora voy a hacer lo mismo para los devs

--- src/app/(Front)/(Private)/dashboard/cliente/page.tsx
+++ src/app/(Front)/(Private)/dashboard/cliente/page.tsx
@@ -20,12 +20,6 @@ const page = async() => {
   
   // user.image es el cargoId
 
-  // const com = '1b16b7b1-df3f-4242-853c-8f12999bb2b8'
-  // const fin = '78509248-2aea-47a3-a4d2-68e210b270cd'
-  // const mar = 'b0665e7d-e4ca-4036-980a-0b22434422dd'
-  // const rec = '658ae10d-5016-48a6-90bf-f2e562fa2bee'
-  // const tec = '447ab6a5-a1c3-4ca9-9de5-ac00f5ecc6bc'
-
   if( user?.image === '1b16b7b1-df3f-4242-853c-8f12999bb2b8'){
     redirect('/dashboard/cliente/comercial')
   }else if(user?.image === '78509248-2aea-47a3-a4d2-68e210b270cd'){
@@ -39,6 +33,9 @@ const page = async() => {
   }
   else if(user?.image === '447ab6a5-a1c3-4ca9-9de5-ac00f5ecc6bc'){
     redirect('/dashboard/cliente/tecnico')
+  }
+  else if(user?.image === '84918a05-cf38-4a5a-a14a-f00a25ef4049'){
+    redirect('/dashboard/cliente/visionario')
   }
   else{
     redirect('/api/auth/login')
---
+++ src/app/(Front)/(Private)/dashboard/cliente/visionario/clienteHistoriasEnviadas/page.tsx
@@ -0,0 +1,124 @@
+'use client'
+import { getDataCompleja } from "@/app/(Front)/React/Fetch/getDataCompleja";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+import { useHistoriaPo } from "@/app/(Front)/(Private)/[stores]/poStore";
+
+const page = () => {
+
+  
+  const { idHistoria, cambiarIdHistoria} = useHistoriaPo()
+
+  const [histouseridcargo, setHistouseridcargo] = useState([])  
+
+
+  useEffect(()=>{
+    const traerHistoriasStatusCargo = async()=>{
+        const ruta = 'historiaStatusCargo' 
+        const param1 = 'a0e8cd01-330c-44da-8efb-52f04217d30b'
+        const param2 = 'Pendiente'
+        const res = await getDataCompleja({ruta, param1, param2})
+        setHistouseridcargo(res)
+    }
+    traerHistoriasStatusCargo()
+  }, [])
+
+  const router = useRouter()
+
+  console.log('serian las histo pendiente del user cliente1:', histouseridcargo);
+  
+  const handleClickVerResolucionHistoria =(id)=>{
+    console.log('idHisto:', id);
+    cambiarIdHistoria(id)
+    console.log('idzusthistoria:', idHistoria);
+    router.push('/dashboard/cliente/verResolucionTarea')
+  }
+
+  return (
+    <div className='w-full h-full   ' >
+        {histouseridcargo.length !== 0 ?
+        <section  className='w-[99%] h-[99%]  '>
+        <main className='py-2 px-4 w-full h-[99%] '>
+            <div className='h-14  bg-violet-100 grid place-content-center '>
+               Historias Enviadas 
+            </div>
+            <header className='w-full h-[7%] -mt-7 flex justify-end items-center  pb-3 font-bold mb-1 pr-6  text-violet-800 '>
+             
+              <div className='pr-3 '>
+                Fecha Actual
+              </div>
+              <div>
+                {format(new Date(), 'dd/MM/yyyy')}
+              </div>
+            </header>
+            <div className='w-[1625px] z-30  top-28 left-3/5 max-h-[563px] overflow-auto'>
+              <table className='border border-gray-200   w-[100%]  '>
+                <thead>
+                  <tr className='h-14'>
+                    <td className='w-[7%] pl-3'>Numero</td>
+                    <td className='w-[20%] pl-8'>Nombre Historia</td>
+                    <td className='w-[10%] pl-8'>Fecha Ingreso</td>
+                    <td className='w-[10%] pl-8'>Hora Ingreso</td>
+                    <td className='w-[12%] pl-8'>Punto Historia</td>
+                    <td className='w-[12%] pl-8'>Tiempo Historia</td>
+                    <td className='w-[12%] pl-8'>Presupuesto Historia </td>
+                    <td className='w-[12%] pl-8'>Status</td>
+                    
+                    
+                  </tr>
+                </thead>
+                <tbody>
+                {histouseridcargo?.map((el, index)=>{
+                  const {id, nombreHistoria, createdAt, status ,updatedAt, horaAt, puntoHistoria, tiempoHistoria, presupuestoHistoria, discrepancia4} = el
+                    const updatedAt2 = format(new Date(updatedAt), 'dd/MM/yyyy')
+                    return <tr key={id} className='border border-gray-200 h-14  cursor-pointer w-full '>
+                      <td className='pl-8'>
+                        {index + 1}
+                      </td>
+                      <td className='pl-8'>
+                        {nombreHistoria}
+                      </td>
+                      <td className='pl-10'>
+                        {createdAt}
+                      </td>
+                      <td className='pl-14'>
+                        {horaAt}
+                      </td>
+                      <td className='pl-20'>
+                        {puntoHistoria}
+                      </td>
+                      <td className='pl-20'>
+                        {tiempoHistoria}
+                      </td>
+                      <td className='pl-20'>
+                        {presupuestoHistoria}
+                      </td>
+                      <td className={`pl-6 ${status === 'Pendiente' ? 'text-yellow-400' : 'text-green-500'}`}>
+                        {status}
+                      </td>
+                      
+                      
+                      
+                      
+                      
+                      
+                    </tr>
+                  })}
+                </tbody>
+            </table>
+      </div>
+      
+        </main>
+    </section>: 
+    <div className='w-full h-full grid content-end justify-center text-xl'>
+      Aún no hay historias Enviadas
+    </div>
+      
+      }
+            
+    </div>
+  )
+}
+
+export default page
